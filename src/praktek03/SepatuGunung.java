3ebe204d5bbf98c0091a35b00067798fa518264e
adriancahaya <32443085+adriancahaya@users.noreply.github.com>
2018-04-08 22:06:58 +0800

    attribute dan method

--- src/praktek03/SepatuGunung.java
+++ src/praktek03/SepatuGunung.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class SepatuGunung extends Sepatu{
+    private String jenisSepatu ;
     
+    void thorttle() {
+        System.out.println("sol");
+        
+    }
 }
