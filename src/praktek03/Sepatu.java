5926a055e9207cb861fc9754345114f3a900d31e
adriancahaya <32443085+adriancahaya@users.noreply.github.com>
2018-04-08 21:15:47 +0800

    buat method cetakinfo

--- src/praktek03/Sepatu.java
+++ src/praktek03/Sepatu.java
@@ -4,4 +4,11 @@ public class Sepatu {
     String merk;
     String warna;
     double harga;
+    
+    void cetakinfo(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                "warna \t: "+warna+"\n"+
+                "harga \t: "+harga);
+        
+    }
 }
